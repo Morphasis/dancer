bf239e62497fd52ebc6e2461e77b8311185046ee
Jack Sharville <jacksharville@gmail.com>
2015-06-17 11:54:59 +0100

    Show message placed.

--- app/controllers/msgs_controller.rb
+++ app/controllers/msgs_controller.rb
@@ -1,13 +1,18 @@
 class MsgsController < ApplicationController
   def new
     @msg = Msg.new
+    @msgs = Msg.all
   end
 
   def create
-    @msg = Msg.new(params[:msg])
+    @msg = Msg.new(msg_params)
     if @msg.save
       redirect_to new_msg_path
     end
   end
+  
+def msg_params
+  params.require(:msg).permit(:name, :emails, :content, :phone)
+end
 
 end
