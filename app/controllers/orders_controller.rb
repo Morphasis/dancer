6d698b00632a8a821d1e8aa2f79a2b877fbca95b
Jack Sharville <jacksharville@gmail.com>
2015-06-24 14:28:03 +0100

    .save bug fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,9 +12,27 @@ class OrdersController < ApplicationController
   end
   
   def order_params
-    params.require(:order).permit(:delivery_name, :company_name, :delivery_address1, :delivery_address2, :delivery_address3, :delivery_city, :delivery_postcode, :delivery_country, :phone, :package_contents, :description_content, :restricted_items, :terms_conditions, :insurance, :contents_value, :cf_reference, :reference_number)
+    params.require(:order).
+      permit(
+        :delivery_name,
+        :company_name,
+        :delivery_address1,
+        :delivery_address2,
+        :delivery_address3,
+        :delivery_city,
+        :delivery_postcode,
+        :delivery_country,
+        :phone,
+        :package_contents,
+        :description_content,
+        :restricted_items,
+        :terms_conditions,
+        :insurance,
+        :contents_value,
+        :cf_reference,
+        :reference_number
+        )
   end
-  
   def show
     @user = User.find(params[:id])
   end
