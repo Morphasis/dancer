fe81e2daff747f463518d5d9ef3160bd251ed9d3
Jack Sharville <jacksharville@gmail.com>
2015-08-05 12:41:06 +0200

    improved stuff

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -8,26 +8,26 @@ ActiveAdmin.register Order do
   index do
     selectable_column
     column :id
+    column :user
+    column :name
     column :delivery_name
     column :complete
     column :delivery_address1
-    column :delivery_address2
+    column :service
+    column :insurance
+    column download_links: [:csv]
     actions
   end
 
   csv do
+    column :name
     column :delivery_name
     column :id 
   end
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
+
   permit_params do
-    permitted = [:delivery_name, :company_name, :delivery_address1, :delivery_address2, :delivery_address3, :delivery_city, :delivery_postcode, :delivery_country, :phone, :package_contents, :description_content, :restricted_items, :terms_conditions, :insurance, :contents_value, :cf_reference, :reference_number, :complete]
+    permitted = [:email ,:country ,:city ,:postcode, :address_line_2, :address_line_1 ,:user_id, :name, :delivery_name, :company_name, :delivery_address1, :delivery_address2, :delivery_address3, :delivery_city, :delivery_postcode, :delivery_country, :phone, :package_contents, :description_content, :restricted_items, :terms_conditions, :insurance, :contents_value, :cf_reference, :reference_number, :complete]
     permitted
   end
 end
+# completed_at
